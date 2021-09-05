0a8a0a4251dc9047625760cff50d31bab4aaad0c
Karim Asaad <82425942+KarimAsaad798@users.noreply.github.com>
2021-09-05 18:03:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let facebtn = document.querySelector("#facebook");
+
+
+facebtn.onclick =  function URL() {
+
+    location.href = 'http://www.facebook.com';
+ 
+};
+
+
+
+let twtbtn = document.querySelector("#twitter");
+
+
+twtbtn.onclick =  function URL() {
+
+    location.href = 'http://www.twitter.com';
+ 
+};
+
+let insbtn = document.querySelector("#instagram");
+
+
+insbtn.onclick =  function URL() {
+
+    location.href = 'http://www.instagram.com';
+ 
+};
+
